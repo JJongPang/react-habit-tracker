f7610e861b529fcd772b2e729dc960b735cda41d
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-12-16 18:10:58 +0900

    Updates

---
+++ precache-manifest.b36292c86d4a917669056e0f40a83962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb287cd425b814e0c3856c306efa5bac",
+    "url": "/react-habit-tracker/index.html"
+  },
+  {
+    "revision": "f02c54db977cf9f5b15e",
+    "url": "/react-habit-tracker/static/css/main.4f74895d.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/react-habit-tracker/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react-habit-tracker/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02c54db977cf9f5b15e",
+    "url": "/react-habit-tracker/static/js/main.ef089ee3.chunk.js"
+  },
+  {
+    "revision": "042765e3f71c043ed96c",
+    "url": "/react-habit-tracker/static/js/runtime-main.f843a2cf.js"
+  }
+]);
